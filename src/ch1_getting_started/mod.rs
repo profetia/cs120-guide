0d8bb42c3e56a33068d1e6132537aa9ac2aca3fd
yanglinshu <nicolasvandarkholme@outlook.com>
2023-11-05 13:35:40 +0800

    chore: add code for ch1.3

--- src/ch1_getting_started/mod.rs
+++ src/ch1_getting_started/mod.rs
@@ -1,2 +1,3 @@
 mod s1_asio_on_windows;
 mod s2_recording_sound_with_cpal;
+mod s3_playing_sound_with_rodio;
---
+++ src/ch1_getting_started/s3_playing_sound_with_rodio.rs
@@ -0,0 +1,33 @@
+use cpal::{
+    traits::{DeviceTrait, HostTrait},
+    HostId, SampleRate, SupportedStreamConfig,
+};
+use rodio::{Decoder, OutputStream, Sink};
+use std::fs::File;
+
+fn main() {
+    let host = cpal::host_from_id(HostId::Asio).expect("failed to initialise ASIO host");
+    let device = host
+        .default_input_device()
+        .expect("failed to find input device");
+
+    let default_config = device.default_input_config().unwrap();
+    let config = SupportedStreamConfig::new(
+        1,                 // mono
+        SampleRate(48000), // sample rate
+        default_config.buffer_size().clone(),
+        default_config.sample_format(),
+    );
+
+    let (_stream, handle) = OutputStream::try_from_device_config(&device, config).unwrap();
+    let sink = Sink::try_new(&handle).unwrap();
+
+    let source = Decoder::new(File::open("output.wav").unwrap()).unwrap();
+    sink.append(source);
+    sink.sleep_until_end();
+
+    sink.pause();
+    sink.play();
+
+    sink.clear();
+}
